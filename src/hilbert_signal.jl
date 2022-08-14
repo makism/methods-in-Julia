a76c660c785a3d343457382b19bda2d15e61ff78
Avraam Marimpis <makhsm@gmail.com>
2022-08-14 13:48:33 +0200

    Extract phase.

--- src/hilbert_signal.jl
+++ src/hilbert_signal.jl
@@ -6,8 +6,13 @@ fs = 10;
 τ = range(0.0, 3.0, step=Ts) |> collect
 ω = 2*π*fs;
 χ = sin.(ω*τ) + sin.(1.2*ω*τ);
-env = abs.(hilbert(χ));
+h = hilbert(χ)
+α = abs.(h);
+#ϕ = unwrap(angle.(h));
+ϕ = angle.(h);
+
 
 plot(χ, label="signal")
-plot!(env, label="+ envelope")
-plot!(-env, label="- envelope")
+plot!(ϕ)
+plot!(α, label="+ envelope")
+plot!(-α, label="- envelope")
